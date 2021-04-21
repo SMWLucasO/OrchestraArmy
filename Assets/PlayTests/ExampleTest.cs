210a489e119c08a6a6474e39813bce7cd474b09f
Gerjan <gerjandijkhuis@gmail.com>
2021-04-21 14:58:04 +0200

    tests

--- Assets/PlayTests/ExampleTest.cs
+++ Assets/PlayTests/ExampleTest.cs
@@ -11,6 +11,7 @@ public class ExampleTest
     public void ExampleTestSimplePasses()
     {
         // Use the Assert class to test conditions
+        Assert.IsTrue(true);
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
@@ -20,6 +21,19 @@ public class ExampleTest
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
-        yield return null;
+        
+        //create a new game object
+        var go = new GameObject();
+        //give it a rigid body
+        go.AddComponent<Rigidbody>();
+        
+        //get it's position
+        var originalPosition = go.transform.position.y;
+
+        //wait till the next FixedUpdate fires
+        yield return new WaitForFixedUpdate();
+
+        //assert that the position no longer equals the original one
+        Assert.AreNotEqual(originalPosition, go.transform.position.y);
     }
 }
