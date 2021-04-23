4277601756f3efc33e1fb53c103980d95c2b0214
Gerjan <gerjandijkhuis@gmail.com>
2021-04-23 15:43:24 +0200

    testing

--- Assets/Scripts/OrchestraArmy/Entity/Entities/Player/Player.cs
+++ Assets/Scripts/OrchestraArmy/Entity/Entities/Player/Player.cs
@@ -1,17 +1,42 @@
-﻿using OrchestraArmy.Entity.Controllers;
+﻿using System;
+using OrchestraArmy.Entity.Controllers;
+using OrchestraArmy.Event;
+using OrchestraArmy.Event.Event;
 using UnityEngine;
 
 namespace OrchestraArmy.Entity.Entities.Player
 {
-    public class Player : LivingDirectionalEntity
+    public class Player : LivingDirectionalEntity, IListener<PlayerDeathEvent>, IListener<PlayerDamageEvent>
     {
         
         public ICameraController CameraController;
 
-        public Player()
+
+        void Start()
         {
-            // ...
+            //testing code.
+            EventManager.Bind<PlayerDeathEvent>(this);
+            EventManager.Bind<PlayerDamageEvent>(this);
+            
+            EventManager.Invoke(new PlayerDeathEvent());
+            
+            EventManager.Invoke(new PlayerDamageEvent
+            {
+                HealthLost = 10
+            });
+            
+            EventManager.Unbind<PlayerDeathEvent>(this);
+            EventManager.Unbind<PlayerDamageEvent>(this);
         }
         
+        public void OnEvent(PlayerDeathEvent invokedEvent)
+        {
+            this.EntityData.Stamina += 1;
+        }
+
+        public void OnEvent(PlayerDamageEvent invokedEvent)
+        {
+            this.EntityData.Health -= invokedEvent.HealthLost;
+        }
     }
 }
---
+++ Assets/Scripts/OrchestraArmy/Event/IListener.cs
@@ -0,0 +1,9 @@
+﻿using OrchestraArmy.Event.Event;
+
+namespace OrchestraArmy.Event
+{
+    public interface IListener<in T> where T: IEvent
+    {
+        public void OnEvent(T invokedEvent);
+    }
+}
