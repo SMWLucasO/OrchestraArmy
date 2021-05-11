c7f92f006e9339184b1aad37d94b2aef2d5fef58
timeglitchd <timeglitchd@gmail.com>
2021-05-11 16:19:48 +0200

    fixed player sprite perspective

--- Assets/Scripts/OrchestraArmy/Entity/Entities/Enemies/Enemy.cs
+++ Assets/Scripts/OrchestraArmy/Entity/Entities/Enemies/Enemy.cs
@@ -1,6 +1,6 @@
 ﻿namespace OrchestraArmy.Entity.Entities.Enemies
 {
-    public class Enemy : LivingDirectionalEntity
+    public abstract class Enemy : LivingDirectionalEntity
     {
         public BehaviourStateMachine Behaviour { get; set; }
     }
