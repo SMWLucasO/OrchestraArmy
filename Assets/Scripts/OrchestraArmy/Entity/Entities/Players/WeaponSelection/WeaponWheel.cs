26a798cc6f37a50f94fc40dce583adb39c52411c
Lucas Ouwens <lucasouwens@gmail.com>
2021-05-11 15:35:11 +0200

    Added a weapon switched event to the weapon wheel.

--- Assets/Scripts/OrchestraArmy/Entity/Entities/Players/WeaponSelection/WeaponWheel.cs
+++ Assets/Scripts/OrchestraArmy/Entity/Entities/Players/WeaponSelection/WeaponWheel.cs
@@ -1,3 +1,6 @@
+using OrchestraArmy.Entity.Entities.Players.WeaponSelection.Weapon.Weapons.Factory;
+using OrchestraArmy.Event;
+using OrchestraArmy.Event.Events.Player;
 using UnityEngine;
 
 namespace OrchestraArmy.Entity.Entities.Players.WeaponSelection
@@ -10,6 +13,67 @@ namespace OrchestraArmy.Entity.Entities.Players.WeaponSelection
         /// </summary>
         [field: SerializeField]
         public WeaponWheelPlaceholder CurrentlySelected { get; set; }
+
+        /// <summary>
+        /// Select the next weapon for usage.
+        /// </summary>
+        /// <returns>bool determining if the weapon was switched.</returns>
+        public bool SwitchToNextWeapon()
+        {
+            if (!WeaponIsUnlocked(CurrentlySelected.Next))
+                return false;
+
+            WeaponType prev = CurrentlySelected.WeaponWheelPlaceholderData.WeaponType;
+            WeaponType newWeaponType = CurrentlySelected.Next.WeaponWheelPlaceholderData.WeaponType;
+            
+            // switch weapons
+            CurrentlySelected = CurrentlySelected.Next;
+            
+            ExecuteWeaponSwitchedEvent(prev, newWeaponType);
+
+            return true;
+        }
         
+        /// <summary>
+        /// Select the previous weapon for usage.
+        /// </summary>
+        /// <returns>bool determining if the weapon was switched.</returns>
+        public bool SwitchToPreviousWeapon()
+        {
+            if (!WeaponIsUnlocked(CurrentlySelected.Previous))
+                return false;
+            
+            WeaponType prev = CurrentlySelected.WeaponWheelPlaceholderData.WeaponType;
+            WeaponType newWeaponType = CurrentlySelected.Previous.WeaponWheelPlaceholderData.WeaponType;
+            
+            // switch weapons
+            CurrentlySelected = CurrentlySelected.Previous;
+            
+            // fire event for weapon switch.
+            ExecuteWeaponSwitchedEvent(prev, newWeaponType);
+
+            return true;
+        }
+        
+        /// <summary>
+        /// Check whether the weapon in the weapon placeholder is unlocked.
+        /// </summary>
+        /// <param name="weaponWheelPlaceholder"></param>
+        /// <returns></returns>
+        private bool WeaponIsUnlocked(WeaponWheelPlaceholder weaponWheelPlaceholder) 
+            => weaponWheelPlaceholder.WeaponWheelPlaceholderData.Unlocked;
+
+        /// <summary>
+        /// Execute the event which indicates that the weapon has been switched.
+        /// </summary>
+        /// <param name="previousWeapon"></param>
+        /// <param name="newlySelectedWeapon"></param>
+        private void ExecuteWeaponSwitchedEvent(WeaponType previousWeapon, WeaponType newlySelectedWeapon)
+            => EventManager.Invoke(new PlayerWeaponChangedEvent()
+            {
+                PreviousWeapon = previousWeapon,
+                NewlySelectedWeapon = newlySelectedWeapon
+            });
+
     }
 }
---
+++ Assets/Scripts/OrchestraArmy/Event/Events/Player/PlayerWeaponChangedEvent.cs
@@ -0,0 +1,18 @@
+﻿using OrchestraArmy.Entity.Entities.Players.WeaponSelection.Weapon.Weapons.Factory;
+
+namespace OrchestraArmy.Event.Events.Player
+{
+    public class PlayerWeaponChangedEvent : IEvent
+    {
+        
+        /// <summary>
+        /// The weapon which the player had before switching.
+        /// </summary>
+        public WeaponType PreviousWeapon { get; set; }
+        
+        /// <summary>
+        /// The weapon the player has just selected.
+        /// </summary>
+        public WeaponType NewlySelectedWeapon { get; set; }
+    }
+}
