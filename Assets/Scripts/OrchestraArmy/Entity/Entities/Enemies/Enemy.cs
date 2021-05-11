4288d66aa5a8c21fc73dfdef418bfe66690f2a32
Lucas Ouwens <lucasouwens@gmail.com>
2021-05-11 14:12:27 +0200

    Temporary enemy collision damages player code.

--- Assets/Scripts/OrchestraArmy/Entity/Entities/Enemies/Enemy.cs
+++ Assets/Scripts/OrchestraArmy/Entity/Entities/Enemies/Enemy.cs
@@ -1,5 +1,9 @@
-﻿using OrchestraArmy.Event;
+﻿using System;
+using OrchestraArmy.Entity.Entities.Players;
+using OrchestraArmy.Event;
 using OrchestraArmy.Event.Events.Enemy;
+using OrchestraArmy.Event.Events.Player;
+using UnityEngine;
 
 namespace OrchestraArmy.Entity.Entities.Enemies
 {
@@ -7,9 +11,13 @@ namespace OrchestraArmy.Entity.Entities.Enemies
     {
         public BehaviourStateMachine Behaviour { get; set; }
 
+        public float LastCollisionTime { get; set; }
+        
         protected override void OnEnable()
         {
             base.OnEnable();
+            
+            LastCollisionTime = Time.time;
             
             // register enemy events.
             EventManager.Bind<EnemyDeathEvent>(this);
@@ -21,5 +29,21 @@ namespace OrchestraArmy.Entity.Entities.Enemies
         /// <param name="enemyDeathEvent"></param>
         public void OnEvent(EnemyDeathEvent enemyDeathEvent) 
             => Destroy(gameObject);
+
+        /// <summary>
+        /// Temporary player attacking event.
+        /// </summary>
+        /// <param name="other"></param>
+        public void OnCollisionStay(Collision other)
+        {
+            if (!other.gameObject.TryGetComponent<Player>(out Player player))
+                return;
+            
+            if (!((Time.time - LastCollisionTime) > 1))
+                return;
+            
+            LastCollisionTime = Time.time;
+            EventManager.Invoke(new PlayerDamageEvent() { HealthLost = 10 });
+        }
     }
 }
