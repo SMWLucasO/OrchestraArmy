a05ec9033507aaf0c65d4d0fe7793673dbca847b
dutch-gamer <bramvdleest@gmail.com>
2021-05-13 13:45:46 +0200

    start of aggro/ follow/ intercept

---
+++ Assets/Scripts/OrchestraArmy/Entity/Entities/Behaviour/AggroBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using OrchestraArmy.Entity.Entities.Enemies.Data;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace OrchestraArmy.Entity.Entities.Enemies
+{
+    public class AggroBehaviour : IBehaviourState
+    {
+        /// <summary>
+        /// variables needed for wander behaviour
+        /// </summary>
+        
+        
+        
+        /// <summary>
+        /// Data used by the state.
+        /// </summary>
+        public StateData StateData { get; set; }
+
+        /// <summary>
+        /// Enter this state.
+        /// </summary>
+        public void Enter()
+        {
+            
+        }
+
+        /// <summary>
+        /// Process this state.
+        /// </summary>
+        public void Process(BehaviourStateMachine machine)
+        {
+            
+            
+        }
+
+        /// <summary>
+        /// Exit this state.
+        /// </summary>
+        public void Exit()
+        {
+            
+        }
+    }
+}
