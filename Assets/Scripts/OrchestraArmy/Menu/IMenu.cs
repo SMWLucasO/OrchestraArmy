8f2ccf4e22f56eaff77503b8687ee52f83b0649d
dutch-gamer <bramvdleest@gmail.com>
2021-06-04 11:22:24 +0200

    not finnished menu scripts

---
+++ Assets/Scripts/OrchestraArmy/Menu/IMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OrchestraArmy.Enum;
+
+public interface IMenu 
+{
+    public MenuButtons Selected { get; set; }
+    
+    public MenuButtons[] PosibleButtons { get; set; }
+    
+    publ
+}
