86b81332abcb11ecee525ae997c72b52740053d7
Lucas Ouwens <lucasouwens@gmail.com>
2021-06-07 13:58:04 +0200

    Added final boss prep

---
+++ Assets/Scripts/OrchestraArmy/Entity/Entities/Enemies/Bosses/TheConductor.cs
@@ -0,0 +1,30 @@
+﻿using OrchestraArmy.Entity.Entities.Players.WeaponSelection.Weapon.Weapons.Factory;
+using OrchestraArmy.Event;
+using OrchestraArmy.Event.Events.Enemy;
+
+namespace OrchestraArmy.Entity.Entities.Enemies.Bosses
+{
+    public class TheConductor : Boss
+    {
+        public override WeaponType HittableBy { get; set; }
+            = WeaponType.Sousaphone;
+
+        public override WeaponType WeaponType { get; set; }
+            = WeaponType.Sousaphone;
+
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+        
+        public override void OnEvent(EnemyDeathEvent enemyDeathEvent)
+        {
+            if (enemyDeathEvent.KilledEnemy.GetInstanceID() != GetInstanceID()) return;
+            
+            EventManager.Invoke(new FinalBossDeathEvent());
+            Destroy(gameObject);
+        }
+        
+    }
+}
