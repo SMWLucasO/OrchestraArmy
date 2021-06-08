f5129f840e8fa5cd1ec3edff5b494c75338c03a5
Lucas Ouwens <lucasouwens@gmail.com>
2021-06-08 13:38:42 +0200

    Bug fixes for going to a previously cleared room.

--- Assets/Scripts/OrchestraArmy/Room/Controllers/MonsterRoomController.cs
+++ Assets/Scripts/OrchestraArmy/Room/Controllers/MonsterRoomController.cs
@@ -1,7 +1,23 @@
-﻿namespace OrchestraArmy.Room.Controllers
+﻿using OrchestraArmy.Event;
+using OrchestraArmy.Event.Events.Room;
+
+namespace OrchestraArmy.Room.Controllers
 {
-    public class MonsterRoomController : RoomController
+    public class MonsterRoomController : RoomController, IListener<RoomClearedOfEnemiesEvent>
     {
+        public override void RegisterEvents()
+        {
+            base.RegisterEvents();
+            EventManager.Bind<RoomClearedOfEnemiesEvent>(this);
+        }
         
+        public override void UnregisterEvents()
+        {
+            base.RegisterEvents();
+            EventManager.Unbind<RoomClearedOfEnemiesEvent>(this);
+        }
+
+        public void OnEvent(RoomClearedOfEnemiesEvent invokedEvent) 
+            => RoomManager.Instance.RoomsCleared += 1;
     }
 }
